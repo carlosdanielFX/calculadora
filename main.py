33fe1f4b33fe7524fdb4c9533e889c14f3b50808
carlosdaniel <carlosdanielferreirasantos600@gamil.com>
2022-10-18 19:44:06 -0300

    atualizado

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
 calculo = None
 
 while calculo != 's':
-    calculo = input(" use S para sair: \n use C para continuar: \n ")
+    calculo = input(" use S para sair: \n use C para somar: \n use m para multiplicar: \n ")
     if calculo == 'c':
         s1 = int(input('Digite numero para soma: \n'))
-        op = str(input('digite um operador: \n'))
+        op = input('digite um operador: \n')
         s2 = int(input('digite segundo numero: \n'))
         if op == '+':
             resultado = s1 + s2
             print(f"resultado e {resultado}")
         else:
-            print("operedor ivalido")
-    elif op == '-':
-        resultado = s1 - s2
-        print(f"resultado e {resultado}")
-    else:
-          print("operedor ivalido")
+            print('OPERACAO INVALIDA')    
+    if calculo == 'm':
+        s3 = int(input('Digite numero para multiplicar: \n'))
+        op = input('digite um operador: \n')
+        s4 = int(input('digite segundo numero: \n'))
+        if op == '*':
+            resultado = s3 * s4
+            print(f"resultado e {resultado}")
+        else:
+            print('OPERACAO INVALIDA')    
+
+   
        
