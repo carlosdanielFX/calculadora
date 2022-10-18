6af4328fbce3e478bd2116f458581a81a5078413
carlosdaniel <carlosdanielferreirasantos600@gamil.com>
2022-10-17 21:13:41 -0300

    calculadora_atualizado

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 calculo = None
 
-while calculo != 'e':
-    calculo = input(" use E para sair: \n use S para soma: \n use M para multiplicar: \n")
-    if calculo == 's':
+while calculo != 's':
+    calculo = input(" use S para sair: \n use C para continuar: \n ")
+    if calculo == 'c':
         s1 = int(input('Digite numero para soma: \n'))
+        op = str(input('digite um operador: \n'))
         s2 = int(input('digite segundo numero: \n'))
-        soma = (s1+s2)
-        print(f" seu resultador e {soma}")
-    elif calculo == 'm':
-        m1 = int(input('digite numero para multiplicao: \n'))
-        m2 = int(input('digite segundo numero: \n'))
-        soma = (m1*m2)
-        print(f"seu resultado e {soma}")
+        if op == '+':
+            resultado = s1 + s2
+            print(f"resultado e {resultado}")
+        else:
+            print("operedor ivalido")
+    elif op == '-':
+        resultado = s1 + s2
+        print(f"resultado e {resultado}")
+    else:
+          print("operedor ivalido")
        
